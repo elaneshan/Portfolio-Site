7f1f2e6cbf0c0c24262f03a4e7ca8fc52a1cb646
elaneshan <eeshane@uci.edu>
2026-07-03 13:35:13 -0700

    Delete app/redeploy-site.sh

--- app/redeploy-site.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tmux kill-server
-cd ~/Portfolio-Site
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-tmux new-session -d -s portfolio "cd ~/Portfolio-Site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
